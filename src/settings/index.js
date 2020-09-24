c3649424604286b26a11cecb839e166b7a51062e
zhengyy <zhengyy@calis.edu.cn>
2020-09-24 15:05:17 +0800

    补充初始的基于Stripes的源码

---
+++ src/settings/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Settings } from '@folio/stripes/smart-components';
+import GeneralSettings from './general-settings';
+import SomeFeatureSettings from './some-feature-settings';
+/* eslint-disable camelcase */
+/*
+  STRIPES-NEW-APP
+  Your app's settings pages are defined here.
+  The pages "general" and "some feature" are examples. Name them however you like.
+*/
+
+export default class __componentName__Settings extends React.Component {
+  pages = [
+    {
+      route: 'general',
+      label: <FormattedMessage id="ui-__packageName__.settings.general" />,
+      component: GeneralSettings,
+    },
+    {
+      route: 'somefeature',
+      label: <FormattedMessage id="ui-__packageName__.settings.some-feature" />,
+      component: SomeFeatureSettings,
+    },
+  ];
+
+  render() {
+    return (
+      <Settings {...this.props} pages={this.pages} paneTitle="__displayName__" />
+    );
+  }
+}
